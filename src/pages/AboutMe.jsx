51c851d8aa0d076207a75c1a6d16d1772cfde8ca
DallasGiles <105821196+DallasGiles@users.noreply.github.com>
2024-07-01 19:44:08 -0600

    changing things up maybe

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,8 +4,9 @@ import React from 'react';
 const AboutMe = () => {
   return (
     <section className="aboutMe">
-      <h2 className="">About Me</h2>
-      <p>This is the About Me section.</p>
+      <h2 className="about-name">Dallas Giles</h2>
+      <p className='about-text'>Full stack web developer</p>
+      <p className='about-para'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam omnis natus deserunt pariatur necessitatibus explicabo qui cum provident ducimus perspiciatis. Velit ea sunt excepturi, omnis mollitia labore cum quis voluptate vitae, a nihil, ut laboriosam!</p>
     </section>
   );
 };
